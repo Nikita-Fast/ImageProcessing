02963d30321f74728e1953ae82cf034a6af9413b
Nikita-Fast <breakdownofsanity1404@gmail.com>
2025-02-19 18:30:52 +0300

    первые шаги

---
+++ Source/task1.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import numpy as np
+
+
+def negative(img):
+    # s=L-1-r
+    lut = np.array([255-r for r in range(256)], dtype=np.uint8)
+    return lut[img]
+
+
+def log_transfiguration(img, c):
+    # s = c * log(1+r)
+    lut = np.array([c * np.log(1+r) for r in range(256)], dtype=np.uint8)
+    print(1)
+    return lut[img]
+
+
+if __name__ == '__main__':
+    img = np.array(Image.open('./../Images/IMG20250203105039.png').convert('RGB'))
+    # neg_img = negative(img)
+    # Image.fromarray(neg_img).show()
+
+    log_img = log_transfiguration(img, 3)
+    Image.fromarray(log_img).show()
